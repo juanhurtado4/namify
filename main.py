a141b44047cfbff15b6212f9bf0cce7822601bda
juan hurtado <juanhurtado4@outlook.com>
2017-11-23 01:15:25 -0800

    Create score database

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -21,5 +23,17 @@ class User(db.Model):
         return 'User({!r}, {!r})'.format(self.username, self.password)
 
 class Score(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
-    db.Column(db.integer, db.Foreignkey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)
+    score = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __init__(self, score, user, date=None):
+        self.score = score
+        self.user = user
+        if date == None:
+            date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        self.date = date
+    
+    def __repr__(self):
+        return 'Score {!r}, {!r}'.format(self.score, self.user)
