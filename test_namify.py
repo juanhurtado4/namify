5e21852e2e28350268bd32dc930f791969fbc71b
juan hurtado <juanhurtado4@outlook.com>
2017-11-24 12:38:19 -0800

    Add test for check_hash_pass function

--- test_namify.py
+++ test_namify.py
@@ -1,10 +1,13 @@
 from hash_password import make_hash_pass, check_hash_pass
 import pytest
 
+passwords = ['python', 'javascript', 'computer_science']
+
 def test_make_hash():
-    password1 = 'python'
-    password2 = 'javascript'
-    password3 = 'computer_science'
-    assert type(make_hash_pass(password1)) == str
-    assert type(make_hash_pass(password2)) == str
-    assert type(make_hash_pass(password3)) == str
+    for password in passwords:
+        assert type(make_hash_pass(password)) == str
+
+def test_check_hash():
+    hash_passwords = [make_hash_pass(password) for password in passwords]
+    for password, hash_pass in zip(passwords, hash_passwords):
+        assert check_hash_pass(password, hash_pass)
