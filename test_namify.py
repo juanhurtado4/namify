996806cbc11bbc51a8e4f3a5a019bd7a618219b3
juan hurtado <juanhurtado4@outlook.com>
2017-11-24 13:39:38 -0800

    Implement test for check_signup_pass function

--- test_namify.py
+++ test_namify.py
@@ -1,7 +1,8 @@
 from hash_password import make_hash_pass, check_hash_pass
+from validate_signup import check_signup_pass
 import pytest
 
-passwords = ['python', 'javascript', 'computer_science']
+passwords = ['python!M1', 'web@2B', 'cs14#S']
 
 def test_make_hash():
     for password in passwords:
@@ -11,3 +12,8 @@ def test_check_hash():
     hash_passwords = [make_hash_pass(password) for password in passwords]
     for password, hash_pass in zip(passwords, hash_passwords):
         assert check_hash_pass(password, hash_pass)
+
+def test_check_signup_pass():
+    for password in passwords:
+        assert check_signup_pass(password)
+    assert check_signup_pass('not valid') == False
